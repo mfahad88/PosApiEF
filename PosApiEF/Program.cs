a006fdda00fbb3bebbf9d5c3b3e141d8eb29f588
Muhammad.Fahad <mfahad88@outlook.com>
2019-01-28 12:28:20 +0500

    Add project files.

---
+++ PosApiEF/Program.cs
@@ -0,0 +1,25 @@
+﻿using PosApiEF.Entities;
+using PosApiEF.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PosApiEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dofactoryEntities context = new dofactoryEntities();
+            var repo = new GenericRepository<Customer>(context);
+            List<Customer> list = repo.GetAll().ToList();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.Id);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PosApiEF/Repository/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace PosApiEF.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private dofactoryEntities _context = null;
+        private DbSet<T> table = null;
+        public GenericRepository()
+        {
+            this._context = new dofactoryEntities();
+            table = _context.Set<T>();
+        }
+        public GenericRepository(dofactoryEntities _context)
+        {
+            this._context = _context;
+            table = _context.Set<T>();
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return table.ToList();
+        }
+        public T GetById(object id)
+        {
+            return table.Find(id);
+        }
+        public void Insert(T obj)
+        {
+            table.Add(obj);
+        }
+        public void Update(T obj)
+        {
+            table.Attach(obj);
+            _context.Entry(obj).State = EntityState.Modified;
+        }
+        public void Delete(object id)
+        {
+            T existing = table.Find(id);
+            table.Remove(existing);
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
